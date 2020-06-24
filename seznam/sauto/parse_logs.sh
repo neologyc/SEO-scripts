dbab95f9eec995199b79099d61f82eea801b3865
Neologyc <jaroslav@hlavinka.cz>
2020-06-24 21:29:12 +0200

    Delete parse_logs.sh

--- seznam/sauto/parse_logs.sh
+++
@@ -1,3 +0,0 @@
-# log starého sauto převede na URL
-# cil je unique cil URL
-cat g-all | sed 's/^.*\]\s//' | sed 's/\sHTTP.*$//' | sed 's/http\s/http:\/\//' | sed 's/https\s/https:\/\//' | sed 's/\s\"GET\s//' | uniq > g-all-url-final
